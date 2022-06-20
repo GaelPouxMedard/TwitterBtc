3ae5e5ebbb30555995238f9a26b803bfce0cf723
GaelPouxMedard <gael.poux-medard@univ-lyon2.fr>
2022-06-20 17:22:37 +0200

    Updates

--- Data/SplitData.py
+++ Data/SplitData.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("Data/Bitcoin_tweets.csv")
+data = pd.read_csv("Bitcoin_tweets.csv")
 number_of_chunks = 20
 
 for id, df_i in  enumerate(np.array_split(data, number_of_chunks)):
-    df_i.to_csv(f'Data/Split/Bitcoin_tweets_{id}.csv', sep=";")
+    df_i.to_csv(f'Split/Bitcoin_tweets_{id}.csv', sep=";")
 
 print(data.columns)
